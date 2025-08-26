5c8f0ccf485d3fe17238b3fd8a1ed3441965c153
Alfonso Alexandre <alfonsoalexandre26@gmail.com>
2025-08-26 00:45:56 -0300

    solucion-click

--- eventos.js
+++ eventos.js
@@ -1,3 +1,8 @@
+function saludar(e) {
+    e.stopPropagation();
+    alert("Hola!");
+}
+
 const boton = document.getElementById("boton");
 boton.addEventListener("click", () => {
     alert("Hola! Soy el div");
